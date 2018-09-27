12e18b24d84b0028551933c79a32e1ec0df23a4f
kosta.kiryazov@bmsys.eu <kosta.kiryazov@bmsys.eu>
2018-09-27 18:54:26 +0300

    First version of the first homework

---
+++ HTTP-Protocol-Exercise/HTTP-Protocol-Exercise/RequestParser/ConsoleRequestParser.cs
@@ -0,0 +1,26 @@
+﻿namespace RequestParser
+{
+    using System;
+
+    /// <summary>
+    /// You have been tasked to write a console application that simulates an HTTP Server’s behavior. 
+    /// You will be receiving HTTP requests and printing HTTP responses for them in a custom format.
+    /// You will receive several lines with valid paths.The last part of the path will be the allowed method. Keep reading paths until you receive "END".
+    /// 
+    /// After that, you will receive a HTTP request. You will have to parse the request and return a response based on that request. 
+    /// If the path of the given request CANNOT be found in the received paths or the request method is NOT allowed for the path, the result will be:
+    /// •	Status: 404 Not Found
+    /// •	Response Text: NotFound
+    /// 
+    /// In all other cases the result will be:
+    /// •	Status: 200 OK
+    /// •	Response Text: OK
+    /// </summary>
+    public class ConsoleRequestParser
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ HTTP-Protocol-Exercise/HTTP-Protocol-Exercise/ValidateURL/ConsoleURLValidator.cs
@@ -0,0 +1,26 @@
+﻿namespace ValidateURL
+{
+    using System;
+
+    /// <summary>
+    /// You have been tasked to create a simple Console URL Validator. The validator works with custom rules though. 
+    /// You will receive encoded URL.Decode the URL and validate it.
+    /// •	If the URL is valid, you must break it into its composite particles and print them.
+    /// •	If the URL is invalid, you must print an error.
+    /// </summary>
+    public class ConsoleURLValidator
+    {
+        public static void Main(string[] args)
+        {
+            string url = "http://mysite.com:80/demo/index.aspx";
+
+            var parts = url.Split("://", StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 2)
+            {
+                Console.WriteLine("Invalid URL");
+            }
+
+            Console.WriteLine($"Protocol: {parts[0]}");
+        }
+    }
+}
